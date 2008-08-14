jpostel/FreeBSD-sandbox

commit 5e92b9f2348dc4eb9b447ffd70ef2e1119770981

Ensure the channel is a number and not a range, the 'channel' command can be easily mixed up with 'chanlist' and would give unexpected results by fixing the channel on the first number in the range.
git-svn-id: svn://svn.freebsd.org/base/head@179958 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sbin/ifconfig/ifieee80211.c
@@ -666,9 +666,12 @@ set80211channel(const char *val, int d, int s, const struct afswtch *rafp)
 	memset(&chan, 0, sizeof(chan));
 	if (!isanyarg(val)) {
 		int v, flags;
+		char *ep;
 
 		getchaninfo(s);
-		v = atoi(val);
+		v = strtol(val, &ep, 10);
+		if (val[0] == '\0' || ep[0] != '\0' || errno == ERANGE)
+			errx(1, "invalid channel number");
 		flags = getchannelflags(val, v);
 		if (v > 255) {		/* treat as frequency */
 			mapfreq(&chan, v, flags);

commit 00f7993df4e6d8e8a88c1eb5f6e6ec2ec8183ab4

Fix channel parsing which was broken in r179958, the channel number may be suffixed with :flag and /width.
git-svn-id: svn://svn.freebsd.org/base/head@181722 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

sbin/ifconfig/ifieee80211.c
@@ -670,8 +670,10 @@ set80211channel(const char *val, int d, int s, const struct afswtch *rafp)
 
 		getchaninfo(s);
 		v = strtol(val, &ep, 10);
-		if (val[0] == '\0' || ep[0] != '\0' || errno == ERANGE)
-			errx(1, "invalid channel number");
+		if (val[0] == '\0' || val == ep || errno == ERANGE ||
+		    /* channel may be suffixed with nothing, :flag, or /width */
+		    (ep[0] != '\0' && ep[0] != ':' && ep[0] != '/'))
+			errx(1, "invalid channel specification");
 		flags = getchannelflags(val, v);
 		if (v > 255) {		/* treat as frequency */
 			mapfreq(&chan, v, flags);